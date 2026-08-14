75143dc45268f8e968611b02ec4e078eb46333e9
Jayisywh <jayismebro@gmail.com>
2026-08-15 00:06:25 +0630

    guard and decorator for the authorization process completed

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
-export class loginDto {
+export class LoginDto {
   @IsEmail({}, { message: 'Please provide a valid email address' })
   @IsNotEmpty({ message: 'Email is required to login' })
   email!: string;
